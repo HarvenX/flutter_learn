21275d236ba4182d44e91ff0dd33a5c059454dc4
xiehuawen <rxie-1549@163.com>
2022-04-29 18:41:32 +0800

    事件传递学习

--- lib/new_router_page.dart
+++ lib/new_router_page.dart
@@ -37,7 +37,7 @@ class _StatusChangeState extends State<ChangeStatusText> {
   Widget build(BuildContext context) {
     return Row(
       children: <Widget>[
-        Text("now Status is $_statusInt"),
+        ChildWidget(showString: "now Status is $_statusInt"),
         ElevatedButton(
           child: Text("Button"),
           onPressed: () {
@@ -45,8 +45,49 @@ class _StatusChangeState extends State<ChangeStatusText> {
               _statusInt++;
             });
           },
-        )
+        ),
+        SelfChangeWidget(outClickTime: _statusInt)
       ],
+    );
+  }
+}
+
+class ChildWidget extends StatelessWidget {
+  final String showString;
+
+  ChildWidget({Key? key, this.showString: "Child "}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Text("Child data : $showString");
+  }
+}
+
+class SelfChangeWidget extends StatefulWidget {
+  final int outClickTime;
+  const SelfChangeWidget({Key? key, this.outClickTime = 0}) : super(key: key);
+
+  @override
+  State<SelfChangeWidget> createState() {
+    return _selfState(outClickTime);
+  }
+}
+
+class _selfState extends State<SelfChangeWidget> {
+  int _selfClickTime = 0;
+  int _outClickTime = 0;
+
+  _selfState(this._outClickTime);
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      child: Text("$_outClickTime  click self $_selfClickTime"),
+      onPressed: () {
+        setState(() {
+          _selfClickTime++;
+        });
+      },
     );
   }
 }
